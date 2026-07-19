6b1f379978640289994bef310b6602a5e02d60b0
mateusz-lazar <mat.laz@seznam.cz>
2026-07-19 13:12:41 +0200

    feat: display min and max temperature, add readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Room temperature monitor
+
+Project using an ESP32 and a BMP180 pressure and temperature sensor to monitor the temperature in my room.
+
+## Hardware
+
+- ESP32 DOIT DEVKIT V1
+- BMP180 temperature and pressure sensor
+- Jumper wires
+
+## Notes
+
+- Work in progress
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 Adafruit_BMP085 bmp;
 WebServer server(80);
 
+struct min_max{
+  float min_temp = 100;
+  float max_temp = -100;
+};
+min_max temp_data;
+
 void sensor_init(){
   Serial.begin(115200);
   Serial.println("Connecting to the BMP180 sensor");
@@ -32,11 +38,10 @@ void wifi_init(){
     Serial.println(WiFi.localIP());
   }
 }
-void html_page()
-{
+void html_page(){
   String html = 
   "<html><body>Room temperature sensor<br>"
-    "Current temperature:"
+    "Current temperature: "
     "<span id='temp'>"
       "--"
     "</span> &deg;C"
@@ -44,24 +49,73 @@ void html_page()
       "function temp_fetch(){fetch('/temperature').then(r=>r.text()).then(t=>{"
         "document.getElementById('temp').innerText=t;"
       "})};"
+    "</script>"
+    "<br>Day maximum: "
+    "<span id='maximum'>"
+      "--"
+    "</span> &deg;C"
+    "<script>"
+      "function maximum_fetch(){fetch('/maximum').then(r=>r.text()).then(t=>{"
+        "document.getElementById('maximum').innerText=t;"
+      "})};"
+    "</script>"
+    "<br>Day minimum: "
+    "<span id='minimum'>"
+      "--"
+    "</span> &deg;C"
+    "<script>"
+      "function minimum_fetch(){fetch('/minimum').then(r=>r.text()).then(t=>{"
+        "document.getElementById('minimum').innerText=t;"
+      "})};"
+
       "temp_fetch();"
       "setInterval(temp_fetch, 2000);"
-    "</script>"
+      "maximum_fetch();"
+      "setInterval(maximum_fetch, 2000);"
+      "minimum_fetch();"
+      "setInterval(minimum_fetch, 2000);"
+      "</script>"
+
   "</body></html>";
   server.send(200, "text/html", html);
 }
-void temperature_send()
-{
+void temperature_send(){
   float temperature = bmp.readTemperature();
   server.send(200, "text/plain", String(temperature, 1));
 }
-void setup() {
+void maximum_send(){
+  float temperature = bmp.readTemperature();
+
+  //change min and max temperature if exceeded
+  if (temperature > temp_data.max_temp){
+    temp_data.max_temp = temperature;
+  }
+   if (temperature < temp_data.min_temp){
+    temp_data.min_temp = temperature;
+  }
+   server.send(200, "text/plain", String(temp_data.max_temp, 1));
+}
+void minimum_send(){
+  float temperature = bmp.readTemperature();
+
+  //change min and max temperature if exceeded
+  if (temperature > temp_data.max_temp){
+    temp_data.max_temp = temperature;
+  }
+   if (temperature < temp_data.min_temp){
+    temp_data.min_temp = temperature;
+  }
+   server.send(200, "text/plain", String(temp_data.min_temp, 1));
+}
+void setup(){
   sensor_init();
   wifi_init();
   server.on("/", html_page);
   server.on("/temperature", temperature_send);
+  server.on("/maximum", maximum_send);
+  server.on("/minimum", minimum_send);
   server.begin();
 }
-void loop() {
+void loop(){
   server.handleClient();
 }
