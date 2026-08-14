d8fe664352888b679c6c4cc94947e13995c0b5df
mateusz-lazar <mat.laz@seznam.cz>
2026-08-14 19:22:59 +0200

    feat: added BMP, WiFi and NTP connection timeout

--- src/main.cpp
+++ src/main.cpp
@@ -17,25 +17,41 @@ struct min_max{
 };
 min_max data_struct;
 bool midnight_reset = false;
+struct tm timeinfo;
 
 void sensor_init(){
   Serial.begin(115200);
+
+  int sensor_timeout_counter = 0;
   Serial.println("Connecting to the BMP180 sensor");
   while(!bmp.begin()){
+    if(sensor_timeout_counter >= 100){
+      Serial.println("\nCouldn't connect to the BMP180 sensor, retrying in 15 minutes");
+      break;
+    }
     Serial.print(".");
     delay(100);
+    sensor_timeout_counter++;
   }
-  Serial.println("Connected to the BMP180 sensor");
+  if(bmp.begin()){
+    Serial.println("Connected to the BMP180 sensor");
+  }
 }
 void wifi_init(){
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.println("Connecting to WiFi");
 
+  int wifi_timeout_counter = 0;
+  Serial.println("Connecting to WiFi");
   while(WiFi.status() != WL_CONNECTED)
   {
+    if(wifi_timeout_counter >= 100){
+      Serial.println("\nCouldn't connect to the WiFi network, retrying in 15 minutes");
+      break;
+    }
     Serial.print(".");
     delay(100);
+    wifi_timeout_counter++;
   }
   if(WiFi.status() == WL_CONNECTED){
     Serial.println("\nConnected to the WiFi network");
@@ -44,16 +60,22 @@ void wifi_init(){
   }
 }
 void get_local_time(){
-  struct tm timeinfo;
   configTime(GMT_OFFSET, DST_OFFSET, NTP_ADRESS);
   
+  int NTP_timeout = 0;
   Serial.println("Retrieving time data");
-
   while(!getLocalTime(&timeinfo)) {
+    if(NTP_timeout >= 100){
+      Serial.println("\nCouldn't connect to the NTP network, retrying in 15 minutes");
+      break;
+    }
     Serial.println(".");
     delay(100);
+    NTP_timeout++;
    }
-   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+   if(getLocalTime(&timeinfo)){
+     Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+   }
 }
 void check_midnight_reset(){
   struct tm timeinfo;
@@ -89,11 +111,23 @@ void data_send(){
 }
 void setup(){
   sensor_init();
-  wifi_init();
-  get_local_time();
-  server.on("/", html_page);
-  server.on("/data", data_send);
-  server.begin();
+  if(bmp.begin())
+  {
+    wifi_init();
+  }
+  if(WiFi.status() == WL_CONNECTED && bmp.begin())
+  {
+    get_local_time();
+  }
+  if(WiFi.status() == WL_CONNECTED && bmp.begin() && getLocalTime(&timeinfo)){
+    server.on("/", html_page);
+    server.on("/data", data_send);
+    server.begin();
+  }
+  else{
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    esp_deep_sleep_start();
+  }
 }
 void loop(){
   server.handleClient();
